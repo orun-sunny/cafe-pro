0197566eebc8209ece6a8e4726e302faa9281632
orun-sunny <sunny.mukut@gmail.com>
2024-08-14 19:53:31 +0600

    first commit

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import Blog from "../Blog/Blog";
+import PropTypes from "prop-types";
+
+const Blogs = ({ handleAddToBookmark }) => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("blog.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
+  return (
+    <div className="md:w-2/3">
+      <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+      {blogs.map((blog) => (
+        <Blog
+          key={blog.id}
+          blog={blog}
+          handleAddToBookmark={handleAddToBookmark}
+          //   handleMarkAsRead={handleMarkAsRead}
+        ></Blog>
+      ))}
+    </div>
+  );
+};
+Blogs.prototypes = {
+  handleAddToBookmark: PropTypes.function,
+};
+
+export default Blogs;
